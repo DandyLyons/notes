5ef99bc8128b9b09f6222462965fce9048ea17e0
Daniel Lyons <72824209+DandyLyons@users.noreply.github.com>
2025-04-26 09:20:05 -0600

    restore quartz.layout.ts
    I accidentally replaced my customized UI layout. This restores my customization.

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -1,6 +1,21 @@
 import { PageLayout, SharedLayout } from "./quartz/cfg"
 import * as Component from "./quartz/components"
 
+// see: https://quartz.jzhao.xyz/features/explorer
+const MyExplorer = Component.Explorer({
+  title: "Explorer",
+  folderClickBehavior: "link",
+  folderDefaultState: "collapsed",
+  useSavedState: true,
+  // sortFn: (a, b) => {
+    // implement sortFn here
+  // },
+  // filterFn: (node) => node.name !== "tags", //filter out "tags" folder
+  // mapFn: undefined,
+  order: ["filter", "map", "sort"], // what order to apply functions in
+})
+
+
 // components shared across all pages
 export const sharedPageComponents: SharedLayout = {
   head: Component.Head(),
@@ -28,6 +43,9 @@ export const defaultContentPageLayout: PageLayout = {
   left: [
     Component.PageTitle(),
     Component.MobileOnly(Component.Spacer()),
+    Component.Search(),
+    Component.Darkmode(),
+    Component.DesktopOnly(MyExplorer),
     Component.Flex({
       components: [
         {
